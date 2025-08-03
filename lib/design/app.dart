2ef3ca9c40c5c84349c452994b868eee3277781e
0viKhan <hossain15-5176@diu.edu.bd>
2025-08-04 02:08:56 +0600

    Initial commit

---
+++ lib/design/app.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+import 'package:task_manager/screejn/splash_screen.dart';
+
+class Taskmanager extends StatelessWidget {
+  const Taskmanager({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: SplashScreen(),
+
+    );
+  }
+}
